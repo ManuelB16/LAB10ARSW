e8ad53ec6ad88eb034d068dd9cbe6c663021048f
Manuel Felipe Barrera Barrera <felipebarrera104@gmail.com>
2025-04-24 07:59:36 -0500

    Update index.js

--- FunctionProject/Fibonacci/index.js
+++ FunctionProject/Fibonacci/index.js
@@ -1,27 +1,30 @@
 var bigInt = require("big-integer");
+
+function fiboM(n, M) {
+    return n <= 1 ? bigInt(n) : fiboMemo(n - 1, M).add(fiboMemo(n - 2, M));
+}
+
+function fiboMemo(n, M) {
+    if (M[n] !== undefined) {
+        return M[n];
+    }
+    M[n] = fiboM(n, M);
+    return M[n];
+}
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    let nth = req.body.nth
-    let nth_1 = bigInt.one;
-    let nth_2 = bigInt.zero;
-    let answer = bigInt.zero;
+    let nth = req.body.nth;
+    var M = {};
 
-    if (nth < 0)
-        throw 'must be greater than 0'
-    else if (nth === 0)
-        answer = nth_2
-    else if (nth === 1)
-        answer = nth_1
-    else {
-        for (var i = 0; i < nth - 1; i++) {
-            answer = nth_2.add(nth_1)
-            nth_2 = nth_1
-            nth_1 = answer
-        }
+    if (nth < 0) {
+        throw 'must be greater than 0';
     }
+
+    let answer = fiboMemo(nth, M);
 
     context.res = {
         body: answer.toString()
     };
-}
+};
